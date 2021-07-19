1ec5a8233fe6d23a4c287ea8433a80ecb925360c
Tadiyos Begashaw <tadiyosbanti@gmail.com>
2021-07-19 15:13:54 +0300

    error handling of auth

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -3,18 +3,39 @@ import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
+import '../models/http_exception.dart';
+
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url = Uri.parse(
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyCe0BneUTEZ9LnySWKz4sPE4i2A5o1X_68');
+    try {
+      final response = await http.post(url,
+          body: json.encode({
+            'email': email,
+            'password': password,
+            'returnSecureToken': true
+          }));
+      final responsedata = json.decode(response.body);
+      if (responsedata['error'] != null) {
+        throw HttpException(responsedata['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
 
   Future<void> signup(String email, String password) async {
     print('signup is called');
-    final url = Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCe0BneUTEZ9LnySWKz4sPE4i2A5o1X_68');
-    final response = await http.post(url,
-        body: json.encode(
-            {'email': email, 'password': password, 'returnSecureToken': true}));
-    print(json.decode(response.body));
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    print('login is called');
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
